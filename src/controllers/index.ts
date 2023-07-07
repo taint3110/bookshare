a902dfb5b28937342e82d6e7a88aa44ef65c3911
Thành Tài <taint0812@gmail.com>
2023-07-07 19:52:00 +0700

    wip

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,7 +2,6 @@ export * from './book-book-category.controller';
 export * from './book-media.controller';
 export * from './book-series.controller';
 export * from './category-book-category.controller';
-export * from './order-book.controller';
 export * from './series-book.controller';
 export * from './series-media.controller';
 export * from './user-order.controller';
